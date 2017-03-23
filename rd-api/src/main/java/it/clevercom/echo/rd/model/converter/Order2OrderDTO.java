fe172a9bcf352c6bcfc4434075d0451fa822a0ab
Luca <l.martini@nectelecomunicazioni.it>
2017-03-23 17:32:48 +0100

    new custom converter order2orderDTO

--- rd-api/src/main/java/it/clevercom/echo/rd/model/converter/Order2OrderDTO.java
+++ rd-api/src/main/java/it/clevercom/echo/rd/model/converter/Order2OrderDTO.java
@@ -1,5 +1,8 @@
 package it.clevercom.echo.rd.model.converter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.dozer.CustomConverter;
 import org.dozer.Mapper;
 import org.dozer.MapperAware;
@@ -9,7 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import it.clevercom.echo.rd.model.dto.BaseObjectDTO;
 import it.clevercom.echo.rd.model.dto.OrderDTO;
 import it.clevercom.echo.rd.model.dto.OrderLogDTO;
+import it.clevercom.echo.rd.model.dto.ServiceDTO;
+import it.clevercom.echo.rd.model.dto.WorkSessionDTO;
 import it.clevercom.echo.rd.model.entity.Order;
+import it.clevercom.echo.rd.model.entity.OrderLog;
+import it.clevercom.echo.rd.model.entity.OrderService;
 
 public class Order2OrderDTO implements CustomConverter, MapperAware {
 	@Autowired
@@ -39,41 +46,55 @@ public class Order2OrderDTO implements CustomConverter, MapperAware {
 			target.setDuration((source.getDuration() != null) ? source.getDuration() : null);
 			target.setIdorder((source.getIdorder() != null) ? source.getIdorder() : null);
 			target.setNotes((source.getNotes() != null) ? source.getNotes() : null);			
-			target.setOrderreason((source.getOrderreason() != null) ? source.getOrderreason() : null);			
-			
-			
+			target.setOrderreason((source.getOrderreason() != null) ? source.getOrderreason() : null);
 			target.setRejectreason((source.getRejectreason() != null) ? source.getRejectreason() : null);
 			target.setRequestingphysician(source.getRequestingphysician() != null ? source.getRequestingphysician() : null);
 			target.setScheduleddate((source.getScheduleddate() != null) ? source.getScheduleddate().getTime() : null);
 			
 			// map related complex object
-
-//			// iterate
-//			target.setOrderLogs(rdDozerMapper.map(source.getOrderLogs(), OrderLogDTO.class));
-//			
-//			// origin org unit
-//			if (source.getOrganizationUnitByOriginorganizationunitid() != null) {
-//				target.setOriginOrganizationUnit(rdDozerMapper.map(source.getOrganizationUnitByOriginorganizationunitid(), BaseObjectDTO.class));
-//			}
-//			
-//			// target org unit
-//			if (source.getOrganizationUnitByTargetorganizationunitid() != null) {
-//				target.setTargetOrganizationUnit(rdDozerMapper.map(source.getOrganizationUnitByTargetorganizationunitid(), BaseObjectDTO.class));
-//			}
-//			
-//			// iterate
-//			target.setServices(services);
-//			
-//			target.setWorkPriority(workPriority);
-//			
-//			if (source.getWorkSession() != null) {
-//				target.setWorkSession(rdDozerMapper.map(source.getWorkSession(), WorkSessionDTO.class));
-//			}
-//			
-//			if (source.getWorkStatus() != null) {
-//				target.setWorkStatus(rdDozerMapper.map(source.getWorkStatus(), BaseObjectDTO.class));
-//			}
 			
+			// origin org unit
+			if (source.getOrganizationUnitByOriginorganizationunitid() != null) {
+				target.setOriginOrganizationUnit(rdDozerMapper.map(source.getOrganizationUnitByOriginorganizationunitid(), BaseObjectDTO.class));
+			}
+			
+			// target org unit
+			if (source.getOrganizationUnitByTargetorganizationunitid() != null) {
+				target.setTargetOrganizationUnit(rdDozerMapper.map(source.getOrganizationUnitByTargetorganizationunitid(), BaseObjectDTO.class));
+			}
+			
+			// workpriority
+			if (source.getWorkPriority() != null)  {
+				target.setWorkPriority(rdDozerMapper.map(source.getWorkPriority(), BaseObjectDTO.class));
+			}
+			
+			// worksession
+			if (source.getWorkSession() != null) {
+				target.setWorkSession(rdDozerMapper.map(source.getWorkSession(), WorkSessionDTO.class));
+			}
+
+			// workstatus
+			if (source.getWorkStatus() != null) {
+				target.setWorkStatus(rdDozerMapper.map(source.getWorkStatus(), BaseObjectDTO.class));
+			}
+			
+			// iterate
+			Set<OrderService> orderServices = source.getOrderServices();
+			if ((orderServices != null) || (!(orderServices.isEmpty()))) {
+				target.setServices(new HashSet<BaseObjectDTO>());
+				for (OrderService orderService : orderServices) {
+					target.getServices().add(rdDozerMapper.map(orderService.getService(), BaseObjectDTO.class));
+				}
+			}
+						
+			// iterate
+			Set<OrderLog> orderLogs = source.getOrderLogs();
+			if ((orderLogs != null) || (!(orderLogs.isEmpty()))) {
+				target.setOrderLogs(new HashSet<OrderLogDTO>());
+				for (OrderLog orderLog : orderLogs) {
+					target.getOrderLogs().add(rdDozerMapper.map(orderLog, OrderLogDTO.class));
+				}
+			}
 			
 			// inject tech fields
 			target.setActive(source.getActive());
