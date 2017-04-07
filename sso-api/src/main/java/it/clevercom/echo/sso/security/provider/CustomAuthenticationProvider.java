7025a4855f96c52b9cdd6467ee63a02ed6ef3716
Luca <l.martini@nectelecomunicazioni.it>
2017-04-07 08:27:11 +0200

    added LDAP integration

--- sso-api/src/main/java/it/clevercom/echo/sso/security/provider/CustomAuthenticationProvider.java
+++ sso-api/src/main/java/it/clevercom/echo/sso/security/provider/CustomAuthenticationProvider.java
@@ -1,5 +1,12 @@
 package it.clevercom.echo.sso.security.provider;
 
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -33,7 +40,22 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 			throw new UsernameNotFoundException(String.format("Lo user '%s' non ha alcuna utenza registrata per questa applicazione", castedAuthentication.getPrincipal()));
 		}
 
-		if (!passwordEncoder.matches(castedAuthentication.getCredentials(), appLogin.getLogin().getPassword())) {
+		if (appLogin.getLogin().getAdlogon()==1) {
+			// Set up the environment for creating the initial context
+			Hashtable<String, String> env = new Hashtable<String, String>();
+			env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+			env.put(Context.PROVIDER_URL, "ldap://10.40.23.13:389/DC=hsangiovanni,DC=roma,DC=it");
+			env.put(Context.SECURITY_AUTHENTICATION, "simple");
+			env.put(Context.SECURITY_PRINCIPAL, appLogin.getLogin().getUsername());
+			env.put(Context.SECURITY_CREDENTIALS, authentication.getCredentials().toString());
+
+			// Create the initial context
+			try {
+				DirContext ctx = new InitialDirContext(env);
+			} catch (Exception e) {
+				throw new BadCredentialsException(String.format("L'autenticazione su LDAP non è riuscita. Password errata per lo username '%s'.", castedAuthentication.getPrincipal()));
+			}
+		} else if (!passwordEncoder.matches(castedAuthentication.getCredentials(), appLogin.getLogin().getPassword())) {
 			throw new BadCredentialsException(String.format("Password errata per lo username '%s'.", castedAuthentication.getPrincipal()));
 		}
 
