df5da6ca5b3d0a20cbac3badab4646274b226360
Alessio Matteo <a.matteo@nectelecomunicazioni.it>
2017-04-27 17:42:35 +0200

    WorkReportDTO and WorkReport2WorkReportDTO definition

---
+++ rd-api/src/main/java/it/clevercom/echo/rd/model/converter/WorkReport2WorkReportDTO.java
@@ -0,0 +1,108 @@
+package it.clevercom.echo.rd.model.converter;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.dozer.CustomConverter;
+import org.dozer.Mapper;
+import org.dozer.MapperAware;
+import org.dozer.MappingException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.clevercom.echo.rd.model.dto.BaseObjectDTO;
+import it.clevercom.echo.rd.model.dto.UserDTO;
+import it.clevercom.echo.rd.model.dto.WorkReportDTO;
+import it.clevercom.echo.rd.model.dto.WorkTaskDTO;
+import it.clevercom.echo.rd.model.entity.WorkReport;
+import it.clevercom.echo.rd.model.entity.WorkReportUser;
+import it.clevercom.echo.rd.model.entity.WorkStatus;
+import it.clevercom.echo.rd.model.entity.WorkTask;
+
+public class WorkReport2WorkReportDTO implements CustomConverter, MapperAware {
+	@Autowired
+	private Mapper rdDozerMapper;
+
+	@Override
+	public Object convert(Object destinationFieldValue, Object sourceFieldValue, Class<?> destinationClass,	Class<?> sourceClass) {
+		if (sourceFieldValue == null) {
+			return null;
+		}
+		
+		if (sourceFieldValue instanceof WorkReport) {
+			WorkReportDTO target = null; 
+			WorkReport source = (WorkReport) sourceFieldValue;
+			
+			// check to see if the object already exists
+			if (destinationFieldValue == null) {
+				target = new WorkReportDTO();
+			} else {
+				target = (WorkReportDTO) destinationFieldValue;
+			}
+			
+			target.setIdWorkReport(source.getIdworkreport());
+			target.setAccessionNumber(source.getAccessionnumber());			
+			target.setWorkStatus(rdDozerMapper.map(source.getWorkStatus(), BaseObjectDTO.class));
+			target.setWorkTask(rdDozerMapper.map(source.getWorkStatus(), WorkTaskDTO.class));
+			target.setCreationDate((source.getCreationdate() != null) ? Long.valueOf(source.getCreationdate().getTime()) : null);
+			target.setCompletionDate((source.getCompletiondate() != null) ? Long.valueOf(source.getCompletiondate().getTime()) : null);
+			target.setBody(source.getBody());
+			if ((source.getWorkReportUsers() != null) && (source.getWorkReportUsers().size() > 0)) {
+				Set<UserDTO> users = new HashSet<UserDTO>();
+				for (WorkReportUser element : source.getWorkReportUsers()) {
+					users.add(rdDozerMapper.map(element.getUser(), UserDTO.class));
+				}
+				target.setWorkReportUsers(users);
+			}
+			
+			target.setActive(source.getActive());
+			target.setCreated(source.getCreated());
+			target.setUpdated(source.getUpdated());
+			target.setUserupdate(source.getUserupdate());
+			
+			return target;
+		} else if (sourceFieldValue instanceof WorkReportDTO) {
+			WorkReport target = null; 
+			WorkReportDTO source = (WorkReportDTO) sourceFieldValue;
+			
+			// check to see if the object already exists
+			if (destinationFieldValue == null) {
+				target = new WorkReport();
+			} else {
+				target = (WorkReport) destinationFieldValue;
+			}
+			
+			target.setIdworkreport(source.getIdWorkReport());
+			
+			target.setWorkStatus(rdDozerMapper.map(source.getWorkStatus(), WorkStatus.class));
+			target.setWorkTask(rdDozerMapper.map(source.getWorkTask(), WorkTask.class));
+			target.setAccessionnumber(source.getAccessionNumber());
+			target.setCreationdate((source.getCreationDate()!=0) ? new Date(source.getCreationDate()) : null);
+			target.setCompletiondate((source.getCompletionDate()!=0) ? new Date(source.getCompletionDate()) : null);
+			target.setBody(source.getBody());
+			// FIXME
+			if ((source.getWorkReportUsers() != null) && (source.getWorkReportUsers().size() > 0)) {
+				Set<WorkReportUser> users = new HashSet<WorkReportUser>();
+				for (UserDTO element : source.getWorkReportUsers()) {
+					users.add(rdDozerMapper.map(element, WorkReportUser.class));
+				}
+				target.setWorkReportUsers(users);
+			}
+			
+			target.setActive(source.getActive());
+			target.setCreated(source.getCreated());
+			target.setUpdated(source.getUpdated());
+			target.setUserupdate(source.getUserupdate());
+			
+			return target;
+		} else {
+			throw new MappingException("Converter WorkReport2WorkReportDTO " + "used incorrectly. Arguments passed in were:" + destinationFieldValue + " and " + sourceFieldValue);
+		}
+	}
+	
+	@Override
+	public void setMapper(Mapper mapper) {
+		// TODO Auto-generated method stub
+		rdDozerMapper = mapper;
+	}
+}
--- rd-api/src/main/java/it/clevercom/echo/rd/model/dto/WorkReportDTO.java
+++ rd-api/src/main/java/it/clevercom/echo/rd/model/dto/WorkReportDTO.java
@@ -1,5 +1,203 @@
 package it.clevercom.echo.rd.model.dto;
 
-public class WorkReportDTO {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import it.clevercom.echo.common.dto.AbstractEchoDTO;
+
+@JsonIgnoreProperties({"created","updated","userupdate","active","idd"})
+public class WorkReportDTO extends AbstractEchoDTO implements Serializable {
+
+	private static final long serialVersionUID = 4871642167804795055L;
+	
+	public Long idWorkReport;
+	public Long accessionNumber;
+	public BaseObjectDTO workStatus;
+	public WorkTaskDTO workTask;
+	private Long creationDate;
+	private Long completionDate;
+	private String body;
+	private Set<UserDTO> workReportUsers;
+	
+	private Date created;
+	private Date updated;
+	private String userupdate;
+	private Boolean active;
+	
+	@Override
+	public Object getIdd() {
+		return idWorkReport;
+	}
+
+	/**
+	 * @return the idWorkReport
+	 */
+	public Long getIdWorkReport() {
+		return idWorkReport;
+	}
+
+	/**
+	 * @param idWorkReport the idWorkReport to set
+	 */
+	public void setIdWorkReport(Long idWorkReport) {
+		this.idWorkReport = idWorkReport;
+	}
+
+	/**
+	 * @return the accessionNumber
+	 */
+	public Long getAccessionNumber() {
+		return accessionNumber;
+	}
+
+	/**
+	 * @param accessionNumber the accessionNumber to set
+	 */
+	public void setAccessionNumber(Long accessionNumber) {
+		this.accessionNumber = accessionNumber;
+	}
+
+	/**
+	 * @return the workStatus
+	 */
+	public BaseObjectDTO getWorkStatus() {
+		return workStatus;
+	}
+
+	/**
+	 * @param workStatus the workStatus to set
+	 */
+	public void setWorkStatus(BaseObjectDTO workStatus) {
+		this.workStatus = workStatus;
+	}
+
+	/**
+	 * @return the workTask
+	 */
+	public WorkTaskDTO getWorkTask() {
+		return workTask;
+	}
+
+	/**
+	 * @param workTask the workTask to set
+	 */
+	public void setWorkTask(WorkTaskDTO workTask) {
+		this.workTask = workTask;
+	}
+
+	/**
+	 * @return the creationDate
+	 */
+	public Long getCreationDate() {
+		return creationDate;
+	}
+
+	/**
+	 * @param creationDate the creationDate to set
+	 */
+	public void setCreationDate(Long creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	/**
+	 * @return the completionDate
+	 */
+	public Long getCompletionDate() {
+		return completionDate;
+	}
+
+	/**
+	 * @param completionDate the completionDate to set
+	 */
+	public void setCompletionDate(Long completionDate) {
+		this.completionDate = completionDate;
+	}
+
+	/**
+	 * @return the body
+	 */
+	public String getBody() {
+		return body;
+	}
+
+	/**
+	 * @param body the body to set
+	 */
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	/**
+	 * @return the workReportUsers
+	 */
+	public Set<UserDTO> getWorkReportUsers() {
+		return workReportUsers;
+	}
+
+	/**
+	 * @param workReportUsers the workReportUsers to set
+	 */
+	public void setWorkReportUsers(Set<UserDTO> workReportUsers) {
+		this.workReportUsers = workReportUsers;
+	}
+
+	/**
+	 * @return the created
+	 */
+	public Date getCreated() {
+		return created;
+	}
+
+	/**
+	 * @param created the created to set
+	 */
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	/**
+	 * @return the updated
+	 */
+	public Date getUpdated() {
+		return updated;
+	}
+
+	/**
+	 * @param updated the updated to set
+	 */
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+	/**
+	 * @return the userupdate
+	 */
+	public String getUserupdate() {
+		return userupdate;
+	}
+
+	/**
+	 * @param userupdate the userupdate to set
+	 */
+	public void setUserupdate(String userupdate) {
+		this.userupdate = userupdate;
+	}
+
+	/**
+	 * @return the active
+	 */
+	public Boolean getActive() {
+		return active;
+	}
+
+	/**
+	 * @param active the active to set
+	 */
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
 
 }
