b07783acd1167f7084fbe8e9a4701a4c34c97f1b
Luca <l.martini@nectelecomunicazioni.it>
2017-05-17 13:45:55 +0200

    refactored

--- echo-common/src/main/java/it/clevercom/echo/common/jpa/UpdateRequestProcessor.java
+++ echo-common/src/main/java/it/clevercom/echo/common/jpa/UpdateRequestProcessor.java
@@ -3,8 +3,6 @@ package it.clevercom.echo.common.jpa;
 import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -17,23 +15,37 @@ import it.clevercom.echo.common.dto.AbstractEchoDTO;
 import it.clevercom.echo.common.exception.model.RecordNotFoundException;
 import it.clevercom.echo.common.jpa.entity.AbstractJpaEchoEntity;
 import it.clevercom.echo.common.model.dto.response.UpdateResponseDTO;
+import it.clevercom.echo.common.model.factory.ResponseFactory;
 
 public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends AbstractJpaEchoEntity, D extends AbstractEchoDTO> {
-	
+	// update operation objects
 	private I repository;
-	private D sourceDTO; // maybe not in use
+	private D sourceDTO;
+	
+	// entities
+	private E newValueEntity;
 	private E oldValueEntity;
-	private DozerBeanMapper mapper;
-	private Class<D> dtoClazz;
-	private Class<I> repoClazz;
+	
+	// dtos
+	private D oldValueDTO;
+	private D newValueDTO;
+	
+	// 
 	private String entity_name;
 	private String entity_id;
 	private String updatedUser;
-	private Environment env;
+	
+	// generic types
 	private Class<?> idClazz;
+	private Class<D> dtoClazz;
+	private Class<I> repoClazz;
+	
+	// system objects
+	private Environment env;
 	private EntityManager em;
 	private final Logger logger = Logger.getLogger(this.getClass());
-
+	private DozerBeanMapper mapper;
+	
 	/**
 	 * 
 	 * @param repository
@@ -80,22 +92,20 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 	 */
 	public UpdateRequestProcessor(I repository, DozerBeanMapper mapper, String entity_name, String entity_id, Environment env, EntityManager em) {
 		super();
-		// repository
+		
+		// set repository
 		this.repository = repository;
-		// mapper
-		this.mapper = mapper;
-		// entity name
+		
+		// set entity info
 		this.entity_name = entity_name;
-		// entity name
 		this.entity_id = entity_id;
-		// set env
+
+		// set system objects
+		this.mapper = mapper;
 		this.env = env;
-		// set entity manager
 		this.em = em;
 		
 		// clazzez
-		//this.dtoClazz = (Class<D>) dto.getClass();
-		//this.idClazz = dto.getIdd().getClass();
 		this.repoClazz = (Class<I>) repository.getClass();
 	}
 	
@@ -105,54 +115,19 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 	 * @throws Exception
 	 */
 	public UpdateResponseDTO<D> process() throws Exception {
-		// get method with reflection
-	    Method method = repoClazz.getMethod("findOne", Serializable.class);
-	    
-	    // cast id to serializable
-	    Serializable id = (Serializable) idClazz.cast(sourceDTO.getIdd());
-	    
-	    // invoke method with reflection and cast result to E
-	    this.oldValueEntity = (E) method.invoke(repository, id);
-	    
-	    // if an entity with given id is not found in DB throw record not found
-	    if (oldValueEntity==null) {
-	    	logger.error(MessageFormat.format(env.getProperty("echo.api.crud.search.noresult"), entity_name, entity_id, id.toString()));
-	    	throw new RecordNotFoundException(entity_name, entity_id, sourceDTO.getIdd().toString());
-	    }
-	    
-		// map old value to a dto
-		D oldValueDTO = mapper.map(oldValueEntity, dtoClazz);
+		// save entity
+		newValueEntity = update();
 		
-		// begin update of oldValue
-		mapper.map(sourceDTO, oldValueEntity);
-
-		// add technical field
-		oldValueEntity.setUserupdate(updatedUser);
-
-		// save and map to out dto
-		E newValueEntity = repository.saveAndFlush(oldValueEntity);
 		// legacy code (should be removed after refactoring)
 		if (em!=null) {
 			em.refresh(newValueEntity);		
 		}
-		D newValueDTO = mapper.map(newValueEntity, dtoClazz);
 		
-		// create standard response
-		UpdateResponseDTO<D> response = new UpdateResponseDTO<D>();
-		response.setEntityName(entity_name);
-		response.setMessage(MessageFormat.format(env.getProperty("echo.api.crud.saved"), entity_name));
-		
-		// add new dtos values
-		List<D> newDTOs = new ArrayList<D>();
-		newDTOs.add(newValueDTO);
-		response.setNewValue(newDTOs);
-		
-		// add old dtos values
-		List<D> oldDTOs = new ArrayList<D>();
-		oldDTOs.add(oldValueDTO);
-		response.setOldValue(oldDTOs);
+		// get new value DTO
+		newValueDTO = mapper.map(newValueEntity, dtoClazz);
 
-		return response;
+		// create standard response
+		return ResponseFactory.getUpdateResponseDTO(oldValueDTO, newValueDTO, entity_name, MessageFormat.format(env.getProperty("echo.api.crud.saved"), entity_name));
 	}
 	
 	/**
@@ -168,7 +143,7 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 	    Serializable id = (Serializable) idClazz.cast(sourceDTO.getIdd());
 	    
 	    // invoke method with reflection and cast result to E
-	    E oldValueEntity = (E) method.invoke(repository, id);
+	    oldValueEntity = (E) method.invoke(repository, id);
 	    
 	    // if an entity with given id is not found in DB throw record not found
 	    if (oldValueEntity==null) {
@@ -177,7 +152,7 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 	    }
 		
 		// map old value to a dto
-		D oldValueDTO = mapper.map(oldValueEntity, dtoClazz);
+		oldValueDTO = mapper.map(oldValueEntity, dtoClazz);
 		
 		// begin update of oldValue
 		mapper.map(sourceDTO, oldValueEntity);
@@ -186,7 +161,7 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 		oldValueEntity.setUserupdate(updatedUser);
 		
 		// save and map to out dto
-		E newValueEntity = repository.saveAndFlush(oldValueEntity);
+		newValueEntity = repository.saveAndFlush(oldValueEntity);
 		
 		// legacy code (should be removed after refactoring)
 		if (em!=null) {
@@ -194,8 +169,8 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 		}
 				
 		return newValueEntity;
-	}	
-	
+	}
+
 	/**
 	 * @param enabled
 	 * @return
@@ -209,7 +184,7 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 	    Serializable id = (Serializable) idClazz.cast(sourceDTO.getIdd());
 	    
 	    // invoke method with reflection and cast result to E
-	    E oldValueEntity = (E) method.invoke(repository, id);
+	    oldValueEntity = (E) method.invoke(repository, id);
 	    
 	    // if an entity with given id is not found in DB throw record not found
 	    if (oldValueEntity==null) {
@@ -218,7 +193,7 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 	    }
 		
 		// map old value to a dto
-		D oldValueDTO = mapper.map(oldValueEntity, dtoClazz);
+		oldValueDTO = mapper.map(oldValueEntity, dtoClazz);
 		
 		// begin update of oldValue
 		mapper.map(sourceDTO, oldValueEntity);
@@ -228,25 +203,11 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 		oldValueEntity.setActive(enabled);
 		
 		// save and map to out dto
-		E newValueEntity = repository.saveAndFlush(oldValueEntity);
-		D newValueDTO = mapper.map(newValueEntity, dtoClazz);
+		newValueEntity = repository.saveAndFlush(oldValueEntity);
+		newValueDTO = mapper.map(newValueEntity, dtoClazz);
 				
 		// create standard response
-		UpdateResponseDTO<D> response = new UpdateResponseDTO<D>();
-		response.setEntityName(entity_name);
-		response.setMessage(MessageFormat.format(env.getProperty("echo.api.crud.saved"), entity_name));
-		
-		// add new dtos values
-		List<D> newDTOs = new ArrayList<D>();
-		newDTOs.add(newValueDTO);
-		response.setNewValue(newDTOs);
-		
-		// add old dtos values
-		List<D> oldDTOs = new ArrayList<D>();
-		oldDTOs.add(oldValueDTO);
-		response.setOldValue(oldDTOs);
-
-		return response;
+		return ResponseFactory.getUpdateResponseDTO(oldValueDTO, newValueDTO, entity_name, MessageFormat.format(env.getProperty("echo.api.crud.saved"), entity_name));
 	}
 
 	/**
@@ -278,4 +239,6 @@ public class UpdateRequestProcessor<I extends JpaRepository<E, ?>, E extends Abs
 	public void setUpdatedUser(String updatedUser) {
 		this.updatedUser = updatedUser;
 	}
+	
+	
 }
--- echo-common/src/main/java/it/clevercom/echo/common/model/factory/ResponseFactory.java
+++ echo-common/src/main/java/it/clevercom/echo/common/model/factory/ResponseFactory.java
@@ -5,9 +5,18 @@ import java.util.List;
 
 import it.clevercom.echo.common.model.dto.response.CreateResponseDTO;
 import it.clevercom.echo.common.model.dto.response.PagedDTO;
+import it.clevercom.echo.common.model.dto.response.UpdateResponseDTO;
 
 public class ResponseFactory {
-
+	/**
+	 * 
+	 * @param dtoList
+	 * @param size
+	 * @param page
+	 * @param totalPages
+	 * @param totalElements
+	 * @return
+	 */
 	public static <T> PagedDTO<T> getPagedDTO(List<T> dtoList, int size, int page, int totalPages, long totalElements) {
 		PagedDTO<T> dto = new PagedDTO<T>();
 		dto.setCurrentPage(page);
@@ -18,6 +27,13 @@ public class ResponseFactory {
 		return dto;
 	}
 	
+	/**
+	 * 
+	 * @param dto
+	 * @param entity_name
+	 * @param message
+	 * @return
+	 */
 	public static <T> CreateResponseDTO<T> getCreateResponseDTO(T dto, String entity_name, String message) {	
 		// create standard response
 		CreateResponseDTO<T> response = new CreateResponseDTO<T>();
@@ -26,6 +42,30 @@ public class ResponseFactory {
 		List<T> dtoS = new ArrayList<T>();
 		dtoS.add(dto);
 		response.setNewValue(dtoS);
+		return response;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static <T> UpdateResponseDTO<T> getUpdateResponseDTO(T oldValueDTO, T newValueDTO, String entityName, String message) {
+		// create standard response
+		UpdateResponseDTO<T> response = new UpdateResponseDTO<T>();
+		
+		response.setMessage(message);
+		response.setEntityName(entityName);
+		
+		// add new dtos values
+		List<T> newDTOs = new ArrayList<T>();
+		newDTOs.add(newValueDTO);
+		response.setNewValue(newDTOs);
+		
+		// add old dtos values
+		List<T> oldDTOs = new ArrayList<T>();
+		oldDTOs.add(oldValueDTO);
+		response.setOldValue(oldDTOs);
+		
 		return response;
 	}
 }
