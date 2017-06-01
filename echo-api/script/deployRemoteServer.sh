8a8efd2f5bafcccc66e5d06200523ced32b2940f
Alessio Matteo <a.matteo@nectelecomunicazioni.it>
2017-06-01 12:19:40 +0200

    Updated links for new internal dev environment
    - Changed maven artifactory repos
    - Changed internal profile links

--- echo-api/script/deployRemoteServer.sh
+++ echo-api/script/deployRemoteServer.sh
@@ -3,5 +3,5 @@
 # this script assumes that you have installed wildfly on path $HOME/bin/wildfly 
 
 artifact=$(ls ../target/*.war)
-echo "deploying artifact $artifact on 192.168.1.240"
-sh $HOME/bin/wildfly/bin/jboss-cli.sh -c controller=192.168.1.240:9990 --user=admin --password=admin --command="deploy $artifact --force"
+echo "deploying artifact $artifact on 192.168.10.242"
+sh $HOME/bin/wildfly/bin/jboss-cli.sh -c controller=192.168.10.242:9995 --user=admin --password=admin --command="deploy $artifact --force"
